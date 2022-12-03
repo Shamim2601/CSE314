65672722ee2c0a517a741a187fbf2c8b27c6ccfe
Shamim2601 <shamim2601@gmail.com>
2022-12-03 20:53:10 +0600

    off1 marking part done

--- Shell/Offline-1/1805060/1805060.sh
+++ Shell/Offline-1/1805060/1805060.sh
@@ -10,5 +10,25 @@ if(($#==2));then
     fi
 fi
 
-echo $max_score>>output.csv
-echo $max_student_id>>output.csv
+echo "student_id,score">output.csv
+cd Submissions
+i=1
+while((i<=$max_student_id))
+do
+if [[ -d "180512$i" ]];then
+    score=$max_score
+    if [[ -f "180512$i/180512$i.sh" ]];then
+        bash 180512$i/180512$i.sh | diff -w ../AcceptedOutput.txt - >180512$i/diff.txt
+        count=$(grep '[<>]' 180512$i/diff.txt | wc -l)
+        score=$((score-count*5))
+        rm 180512$i/diff.txt
+    else
+        score=0
+    fi
+    echo "180512$i,$score">>../output.csv
+else
+    echo "180512$i,0">>../output.csv
+fi
+i=$(($i+1))
+done
+cd ..
