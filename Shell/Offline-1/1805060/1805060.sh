c841a31d2f6c57ae7e6f8c0a23221addf38192e5
Shamim2601 <shamim2601@gmail.com>
2022-12-03 18:31:04 +0600

    h

--- Shell/Offline-1/1805060/1805060.sh
+++ Shell/Offline-1/1805060/1805060.sh
@@ -1,2 +1,14 @@
-#!bin/bash
-echo hello
+#!/bin/bash
+max_score=100
+max_student_id=5
+if(($#==2));then
+    max_score=$1
+    if(($2>=1 && $2<=9));then
+        max_student_id=$2
+    else
+        echo "Value of arg2 should be between 1 and 9."
+    fi
+fi
+
+echo $max_score>>output.csv
+echo $max_student_id>>output.csv
