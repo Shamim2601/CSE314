ffecfe759ddffab09d2763a0d2513eae29be88bf
Shamim2601 <shamim2601@gmail.com>
2022-12-25 00:22:54 +0600

    online1 done

---
+++ Shell/Online-1/players/1805060.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+replacer() {
+    for file in $*; do
+        if [[ -d $file ]]; then
+            cd $file
+            replacer $(ls)
+            cd ..
+        elif [[ "${file##*.}" != "sh" ]]; then
+            player_name=$(cat $file | head -n1)
+            country_name=$(cat $file | head -n2 | tail -n1)
+            role=$(cat $file | head -n4 |tail -n1)
+
+
+
+            pwd
+        fi
+    done
+}
+
+ls
+replacer $(ls)
